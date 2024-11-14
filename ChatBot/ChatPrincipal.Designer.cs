f73adcab3866ea36222a19338942493868c7a578
Santiago Cuervo <santic9999@gmail.com>
2024-11-14 11:28:48 -0500

    Agregar archivos de proyecto.

---
+++ ChatBot/ChatPrincipal.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ChatBot
+{
+    partial class ChatPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usuario = new System.Windows.Forms.TextBox();
+            this.chatbot = new System.Windows.Forms.TextBox();
+            this.enviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usuario
+            // 
+            this.usuario.Location = new System.Drawing.Point(43, 361);
+            this.usuario.Name = "usuario";
+            this.usuario.Size = new System.Drawing.Size(536, 26);
+            this.usuario.TabIndex = 0;
+            // 
+            // chatbot
+            // 
+            this.chatbot.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.chatbot.Location = new System.Drawing.Point(43, 50);
+            this.chatbot.Multiline = true;
+            this.chatbot.Name = "chatbot";
+            this.chatbot.Size = new System.Drawing.Size(688, 290);
+            this.chatbot.TabIndex = 1;
+            // 
+            // enviar
+            // 
+            this.enviar.Location = new System.Drawing.Point(603, 356);
+            this.enviar.Name = "enviar";
+            this.enviar.Size = new System.Drawing.Size(128, 37);
+            this.enviar.TabIndex = 2;
+            this.enviar.Text = "Enviar";
+            this.enviar.UseVisualStyleBackColor = true;
+            this.enviar.Click += new System.EventHandler(this.enviar_Click);
+            // 
+            // ChatPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 415);
+            this.Controls.Add(this.enviar);
+            this.Controls.Add(this.chatbot);
+            this.Controls.Add(this.usuario);
+            this.Name = "ChatPrincipal";
+            this.Text = "Asistente de Registro de Horario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox usuario;
+        public System.Windows.Forms.TextBox chatbot;
+        private System.Windows.Forms.Button enviar;
+    }
+}
+
---
+++ ChatBot/ChatPrincipal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ChatBot
+{
+    public partial class ChatPrincipal : Form
+    {
+        public ChatPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void enviar_Click(object sender, EventArgs e)
+        {
+
+            // Obtener la entrada del usuario desde el TextBox 'usuario'
+            string entradaUsuario = usuario.Text;
+
+            // Realizar la tokenización y stemming
+            List<string> tokens = TokenizarTexto(entradaUsuario);
+            List<string> stemmedTokens = StemTokens(tokens);
+
+            // Mostrar los resultados en el TextBox 'chatbot'
+            chatbot.AppendText($"Entrada del usuario: {entradaUsuario}\n");
+            chatbot.AppendText("Tokens generados: " + string.Join(", ", tokens) + "\n");
+            chatbot.AppendText("Tokens procesados (Stemming): " + string.Join(", ", stemmedTokens) + "\n\n");
+        }
+
+        // Método para tokenizar el texto
+        private List<string> TokenizarTexto(string texto)
+        {
+            // Convertir a minúsculas y dividir el texto en palabras
+            return Regex.Split(texto.ToLower(), "\\W+").Where(t => !string.IsNullOrEmpty(t)).ToList();
+        }
+
+        // Método para realizar stemming en los tokens
+        private List<string> StemTokens(List<string> tokens)
+        {
+            // Simular un proceso de stemming básico (usar librerías como SnowballStemmer en proyectos más avanzados)
+            List<string> stemmed = new List<string>();
+            foreach (var token in tokens)
+            {
+                // Simulación de un stemming simple eliminando sufijos comunes
+                if (token.EndsWith("ing"))
+                {
+                    stemmed.Add(token.Substring(0, token.Length - 3));
+                }
+                else if (token.EndsWith("ed"))
+                {
+                    stemmed.Add(token.Substring(0, token.Length - 2));
+                }
+                else
+                {
+                    stemmed.Add(token);
+                }
+            }
+            return stemmed;
+        }
+    }
+}
